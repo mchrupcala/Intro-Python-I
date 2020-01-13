89410848cb55ea8be6c2cee33eb3887431b191c5
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2020-01-13 17:43:06 -0500

    Totally forgot to fork my project. 10 files finished so far.

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -34,11 +34,13 @@ waypoints = [
 ]
 
 # Add a new waypoint to the list
-# YOUR CODE HERE
-
+waypoints.append({"lat": 50, "long": -115, "name": "I have no idea where this is"})
+print(waypoints)
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
-# YOUR CODE HERE
+waypoints[0]["lon"] = -130
+waypoints[0]["name"] = "not a real place"
 
 # Write a loop that prints out all the field values for all the waypoints
-# YOUR CODE HERE
+for i in waypoints:
+    print(i.keys(), i.values())
